32da8cc8e051330d92f6f1fec679744a0d13050f
XhmikosR <xhmikosr@users.sourceforge.net>
2014-03-23 13:23:54 +0200

    Remove `boss`.

--- js/carousel.js
+++ js/carousel.js
@@ -103,7 +103,7 @@
       $next = this.$element.find('.item')[fallback]()
     }
 
-    if ($next.hasClass('active')) return this.sliding = false
+    if ($next.hasClass('active')) return (this.sliding = false)
 
     var relatedTarget = $next[0]
     var slideEvent = $.Event('slide.bs.carousel', { relatedTarget: relatedTarget, direction: direction })
